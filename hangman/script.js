b53e89f4861c720906408139418e7964e92f7274
MatthewSidoti <coding.sidoti@gmail.com>
2024-06-26 18:34:51 -0400

    starting

---
+++ hangman/script.js
@@ -0,0 +1,15 @@
+// comp picks random ltr
+/*10 guesses if you cant guess letter in 10 turns you lose
+
+a user should see # of wins losses and the current # of turns left
+
+a user shoud be able to click on a letter button to register a guess
+
+when guess is registered we shgoudl show winng message if the guess is correct remove 1 from total guesses
+
+nice to have: allow user to have keyboard inputs as well as the ltr buttons
+    nice to have: disable a button if the choice is already registered.
+
+    Display guesses that were wrong.*/
+
+    
